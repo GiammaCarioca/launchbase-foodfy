f816da19ed66cfecfa998659a2dfddbf5f2ce7e8
Thiago Giammattey <giammattey@gmail.com>
2020-02-06 10:35:49 -0300

    refatora modal

--- scripts.js
+++ scripts.js
@@ -5,20 +5,17 @@ for (let card of cards) {
 	card.addEventListener('click', function(e) {
 		modalOverlay.classList.add('active')
 
-		const receipt = {
-			title: e.target.closest('.card').childNodes[3].textContent,
-			image: e.target.closest('.card').childNodes[1].src,
-			author: e.target.closest('.card').childNodes[5].textContent
-		}
+		const receipt = this.children
 
 		createCard(receipt)
 	})
 }
 
-function createCard(receipt) {
-	modalOverlay.querySelector('img').src = receipt.image
-	modalOverlay.querySelector('h3').innerHTML = receipt.title
-	modalOverlay.querySelector('p').innerHTML = receipt.author
+function createCard([image, title, author]) {
+	modalOverlay.querySelector('img').src = image.src
+	modalOverlay.querySelector('img').alt = title.textContent
+	modalOverlay.querySelector('h3').innerHTML = title.textContent
+	modalOverlay.querySelector('p').innerHTML = author.textContent
 }
 
 document.addEventListener('click', function(e) {
