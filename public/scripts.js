73a91d32f7563956979a04e6d0ecd56ad60c11b0
Thiago Giammattey <giammattey@gmail.com>
2020-02-23 10:57:02 -0300

    ajusta estilos e adiciona link ativo no menu

--- public/scripts.js
+++ public/scripts.js
@@ -23,3 +23,14 @@ for (let button of buttons) {
 		}
 	})
 }
+
+// Menu ativo
+
+const currentPage = location.pathname
+const menuItems = document.querySelectorAll('header .links a')
+
+for (item of menuItems) {
+	if (currentPage.includes(item.getAttribute('href'))) {
+		item.classList.add('active')
+	}
+}
